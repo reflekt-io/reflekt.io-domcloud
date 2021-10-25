241c233da05008790dcc9ab35d4a365aacdc42e8
Loanda Gunawan <loanda.gunawan@ui.ac.id>
2021-10-25 23:01:33 +0700

    Implement views

--- deteksi_depresi/views.py
+++ deteksi_depresi/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from .models import HasilDeteksiDepresi
+from .forms import PHQ9Form
+
+def index(request):
+    if request.method != 'POST':
+        form = PHQ9Form()
+    else:
+        form = PHQ9Form(data=request.POST)
+        if form.is_valid():
+            form_data = form.cleaned_data
+            skor = int(form_data['phq_1']) + int(form_data['phq_2']) + int(form_data['phq_3']) + int(form_data['phq_4']) + int( form_data['phq_5']) + int(form_data['phq_6']) + int(form_data['phq_7']) + int(form_data['phq_8']) + int(form_data['phq_9'])
+
+            if skor >= 20:
+                result = "Depresi berat"
+            elif skor >= 15:
+                result = "Depresi sedang"
+            elif skor >= 10:
+                result = "Depresi ringan"
+            elif skor >= 5:
+                result = "Gejala depresi ringan"
+            else:
+                result = "Tidak terdapat gejala depresi"
+            
+            if request.user.is_authenticated:
+                hasil_sebelum = HasilDeteksiDepresi.objects.filter(owner = request.user).first()
+                if hasil_sebelum:
+                    hasil_sebelum.result = result
+                    hasil_sebelum.save()
+                else:
+                    HasilDeteksiDepresi.objects.create(owner = request.user, result = result)
+                return redirect('deteksi_depresi:index')
+            else:
+                context = {'form': form, 'hasil_deteksi': result, 'tanggal_deteksi': None}
+                return render(request, 'index.html', context)
+
+    hasil_deteksi = None
+    tanggal_deteksi = None
+    if request.user.is_authenticated:
+        hasil = HasilDeteksiDepresi.objects.filter(owner=request.user).first()
+        if hasil:
+            hasil_deteksi = hasil.resultj
+            tanggal_deteksi = hasil.date_added
+
+    context = {'form': form, 'hasil_deteksi': hasil_deteksi, 'tanggal_deteksi': tanggal_deteksi}
+    return render(request, 'index.html', context)
