1bcc9fee4122dc721a0596b413492038a7934b34
determinedguy <mhd.athallah@gmail.com>
2021-10-25 11:13:44 +0700

    Change timezone and fix bug

--- journal/forms.py
+++ journal/forms.py
@@ -3,23 +3,23 @@ from django.forms import widgets
 from journal.models import Journal
 
 # Set choices for radio button parts
-FEELINGS = [('1', 'Sangat Buruk'),
-            ('2', 'Buruk'),
-            ('3', 'Biasa Saja'),
-            ('4', 'Baik'),
-            ('5', 'Sangat Baik')]
+FEELINGS = [(1, 'Sangat Buruk'),
+            (2, 'Buruk'),
+            (3, 'Biasa Saja'),
+            (4, 'Baik'),
+            (5, 'Sangat Baik')]
 
-ANXIETY_RATE = [('0', '0'),
-                ('1', '1'),
-                ('2', '2'),
-                ('3', '3'),
-                ('4', '4'),
-                ('5', '5'),
-                ('6', '6'),
-                ('7', '7'),
-                ('8', '8'),
-                ('9', '9'),
-                ('10', '10')]
+ANXIETY_RATE = [(0, '0'),
+                (1, '1'),
+                (2, '2'),
+                (3, '3'),
+                (4, '4'),
+                (5, '5'),
+                (6, '6'),
+                (7, '7'),
+                (8, '8'),
+                (9, '9'),
+                (10, '10')]
 
 # Define widgets
 class InputMessage(widgets.Textarea):
