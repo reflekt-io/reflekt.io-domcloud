3d2d9563261c7c2c8a3abb4cb5d18bf8e20a6a03
orelar <77086914+orelar@users.noreply.github.com>
2021-10-27 14:50:31 +0800

    Fix Kutipan_Penyemangat

--- kutipan_penyemangat/urls.py
+++ kutipan_penyemangat/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import add_quotes, index
+from main.views import home
+from kutipan_penyemangat.views import add_quotes, index, json_kutipan_penyemangat
 
 app_name = "kutipan_penyemangat"
 
 urlpatterns = [
-    path('', index, name='index-kutipanpenyemangat'),
-    path('add-quotes', add_quotes, name='add-quotes')
+    path('', home),
+    path('', index, name='index-kutipan-penyemangat'),
+    path('add-quotes', add_quotes, name='add-quotes'),
+    path('json', json_kutipan_penyemangat, name='json-kutipan-penyemangat'),
 ]
