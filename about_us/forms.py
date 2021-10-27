35dc5a4d3376728532c3b55b5d4de4f8b2697a7d
Muhammad Bintang Azriel Aditya Wardhana <74897859+bintangazriel@users.noreply.github.com>
2021-10-27 17:55:15 +0800

    Revise About Us' content to be in Indonesian

--- about_us/forms.py
+++ about_us/forms.py
@@ -7,9 +7,9 @@ class ContactForm(forms.ModelForm):
         model = Contact
         fields = "__all__"
         widgets = {
-            'full_name': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Your Full Name', 'required': 'true'}),
-            'query_topic': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Your Query Topic', 'required': 'true'}),
-            'phone_number': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Your Phone Number', 'required': 'true'}),
-            'email': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Your Email Address', 'required': 'true'}),
-            'message': forms.Textarea(attrs={'class':'form-control', 'placeholder':'Your Message', 'required': 'true'}),
+            'full_name': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Nama lengkap Anda', 'required': 'true'}),
+            'query_topic': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Topik pesan Anda', 'required': 'true'}),
+            'phone_number': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Nomor telepon Anda', 'required': 'true'}),
+            'email': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Alamat surel Anda', 'required': 'true'}),
+            'message': forms.Textarea(attrs={'class':'form-control', 'placeholder':'Tuliskan pesan Anda', 'required': 'true'}),
         }
