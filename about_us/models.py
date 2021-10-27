4b1f64f84086d3ed88a8991ff5c3cb7dc279e983
Muhammad Bintang Azriel Aditya Wardhana <74897859+bintangazriel@users.noreply.github.com>
2021-10-27 11:47:00 +0800

    Implement About Us module

--- about_us/models.py
+++ about_us/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    full_name = models.CharField(verbose_name="Full Name", max_length=100, null=True, blank=True)
+    query_topic = models.CharField(verbose_name="Query Topic",max_length=100, null=True, blank=True)
+    phone_number = models.CharField(verbose_name="Phone Number", max_length=15, null=True, blank=True)
+    email = models.CharField(verbose_name="Email",max_length=100, null=True, blank=True)
+    message = models.CharField(verbose_name="Message",max_length=200, null=True, blank=True)
--- about_us/views.py
+++ about_us/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http.response import HttpResponseRedirect
+from .forms import ContactForm
 
 # Create your views here.
+def index(request):
+    form = ContactForm(request.POST or None)
+
+    if (form.is_valid() and request.method == 'POST'):
+        form.save()
+        return HttpResponseRedirect('/about-us')
+        
+    context = {'form':form}
+    return render(request, 'about_index.html', context)
+  
