026186fbc6c5777100bc2784e5e6fcad1520a873
Muhammad Bintang Azriel Aditya Wardhana <74897859+bintangazriel@users.noreply.github.com>
2021-10-27 23:23:48 +0800

    Implement contact submission using AJAX

--- about_us/models.py
+++ about_us/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Contact(models.Model):
-    full_name = models.CharField(verbose_name="Full Name", max_length=100, null=True, blank=True)
-    query_topic = models.CharField(verbose_name="Query Topic",max_length=100, null=True, blank=True)
+    full_name = models.CharField(verbose_name="Full Name", max_length=200, null=True, blank=True)
+    query_topic = models.CharField(verbose_name="Query Topic",max_length=200, null=True, blank=True)
     phone_number = models.CharField(verbose_name="Phone Number", max_length=15, null=True, blank=True)
-    email = models.CharField(verbose_name="Email",max_length=100, null=True, blank=True)
-    message = models.CharField(verbose_name="Message",max_length=200, null=True, blank=True)
+    email = models.CharField(verbose_name="Email",max_length=200, null=True, blank=True)
+    message = models.TextField(verbose_name="Message", null=True, blank=True)
--- about_us/views.py
+++ about_us/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
-from django.http.response import HttpResponseRedirect
+from django.http.response import JsonResponse
 from .forms import ContactForm
 
 # Create your views here.
 def index(request):
-    form = ContactForm(request.POST or None)
+    if request.method == 'POST' and  request.is_ajax():
+        form = ContactForm(request.POST)
+        data = {}
 
-    if (form.is_valid() and request.method == 'POST'):
-        form.save()
-        return HttpResponseRedirect('/about-us')
-        
-    context = {'form':form}
-    return render(request, 'about_us_index.html', context)
+        if form.is_valid():
+            form.save()
+            data['success'] = True
+            return JsonResponse(data)
+        else:
+            data['success'] = False
+            return JsonResponse(data)
+    else:
+        form = ContactForm()
+        return render(request, 'about_us_index.html', {'form':form})
   
