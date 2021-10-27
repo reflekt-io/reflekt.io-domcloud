3a4f0071adcfc787cf382edcc99c802b3033ee3f
determinedguy <mhd.athallah@gmail.com>
2021-10-27 14:25:49 +0700

    Fix navbar and delete unnecessary files

--- kutipan_penyemangat/urls.py
+++ kutipan_penyemangat/urls.py
@@ -4,6 +4,6 @@ from .views import add_quotes, index
 app_name = "kutipan_penyemangat"
 
 urlpatterns = [
-    path('', index, name='index-kutipanpenyemangat'),
+    path('', index, name='index'),
     path('add-quotes', add_quotes, name='add-quotes')
 ]
