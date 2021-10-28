2b9c24382846a93b543661fcba7cc9d64f4dc02e
Muhammad Fikri Haryanto <mfharyantooo@gmail.com>
2021-10-28 19:01:09 +0700

    Update models refleksi

--- refleksi/admin.py
+++ refleksi/admin.py
@@ -1,4 +1,1 @@
 from django.contrib import admin
-from .models import Activity
-
-admin.site.register(Activity)
--- refleksi/models.py
+++ refleksi/models.py
@@ -1,5 +1,1 @@
 from django.db import models
-
-class Activity(models.Model):
-    name = models.CharField(max_length=40)
-    description = models.CharField(max_length=60)
