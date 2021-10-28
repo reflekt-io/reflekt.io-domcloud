b1df03fd5eb934a4e7d2ec01aef2f390f95c62a3
vnctkevin <vnctkevin@gmail.com>
2021-10-28 22:07:34 +0700

    Add ajax function for Curhat Cards

--- pojok_curhat/urls.py
+++ pojok_curhat/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from pojok_curhat.views import index, add_curhat, navbar, curhat_list
+from pojok_curhat.views import index, add_curhat, navbar, curhat_list, json_pojok_curhat
 
 app_name = "pojok_curhat"
 
 urlpatterns = [
     path('', index, name='index'),
+    path('json', json_pojok_curhat, name = 'json_pojok_curhat'),
     path('navbar', navbar, name='navbar'),
     path('add-curhat', add_curhat, name='add_curhat'),
     path('curhat-list', curhat_list, name='curhat_list'),
