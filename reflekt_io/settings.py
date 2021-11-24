26328d1093a5f1ebf33e4d1072cf1bd9a31be970
determinedguy <mhd.athallah@gmail.com>
2021-11-24 07:19:48 +0700

    Try to fix things up

--- reflekt_io/settings.py
+++ reflekt_io/settings.py
@@ -80,7 +80,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',    
 ]
 
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'reflekt_io.urls'
 
