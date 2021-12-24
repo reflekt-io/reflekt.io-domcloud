15630543f9f811e1aa52ad3e83a9c96be7c3a5bf
Loanda Gunawan <loanda.gunawan@ui.ac.id>
2021-12-24 14:23:50 +0700

    Update views.py

--- deteksi_depresi/views.py
+++ deteksi_depresi/views.py
@@ -1,6 +1,7 @@
+import json
 from django.shortcuts import render, redirect
 from django.http import JsonResponse, HttpResponse
-
+from django.views.decorators.csrf import csrf_exempt
 from .models import HasilDeteksiDepresi
 from .forms import PHQ9Form
 
@@ -46,3 +47,33 @@ def get_phq9_json(request):
         else:
             return JsonResponse({'error': form.errors})
     return redirect('deteksi_depresi:phq9')
+
+@csrf_exempt
+def add_result_flutter(request):
+    if request.method == 'POST':
+        if request.user.is_authenticated:
+            data = json.loads(request.body)
+            result = data["result"]
+            
+            hasil_deteksi = HasilDeteksiDepresi.objects.filter(owner=request.user).first()
+            if hasil_deteksi:
+                hasil_deteksi.result = result
+                hasil_deteksi.save()
+            else:
+                hasil_deteksi = HasilDeteksiDepresi(owner=request.user, result=result)
+                hasil_deteksi.save()
+
+        return JsonResponse({"status": "success"}, status=200)
+    else:
+        return JsonResponse({"status": "error"}, status=401)
+
+@csrf_exempt
+def fetch_result_flutter(request):
+    if request.method == 'GET':
+        if request.user.is_authenticated:
+            hasil_deteksi = HasilDeteksiDepresi.objects.filter(owner=request.user).first()
+            if hasil_deteksi:
+                return JsonResponse({"result": hasil_deteksi.result, "date": hasil_deteksi.date}, status=200)
+        return JsonResponse({"result": "", "date": ""}, status=200)
+    else:
+        return JsonResponse({"status": "error"}, status=401)
