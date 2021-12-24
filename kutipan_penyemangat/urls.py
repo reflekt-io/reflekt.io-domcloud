1a315d0cf7e96a3f0e59c342fc22e4085eb55037
Aurelia Amanda Robert <aurelia.amanda@ui.ac.id>
2021-12-24 21:49:22 +0800

    Finish Kutipan on Flutter

--- kutipan_penyemangat/urls.py
+++ kutipan_penyemangat/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from kutipan_penyemangat.views import add_quotes, index, json_kutipan_penyemangat
+from kutipan_penyemangat.views import add_kutipan_flutter, add_quotes, index, json_kutipan_penyemangat
 
 app_name = "kutipan_penyemangat"
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', index, name='index'),
     path('add-quotes', add_quotes, name='add-quotes'),
     path('json', json_kutipan_penyemangat, name='json-kutipan-penyemangat'),
+    path('add-kutipan-flutter',add_kutipan_flutter, name='add-kutipan-flutter')
 ]
