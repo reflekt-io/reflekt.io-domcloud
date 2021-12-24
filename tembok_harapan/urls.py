e23cb830470167547c4ea8da054945dbe6d154bf
Teuku Faiz Aryasena <84432271+teukufaiz@users.noreply.github.com>
2021-12-24 22:16:03 +0700

    add tembok harapan backend flutter

--- tembok_harapan/urls.py
+++ tembok_harapan/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from tembok_harapan.views import index, add_harapan
+from tembok_harapan.views import *
 
 app_name = 'tembok_harapan'
 
 urlpatterns = [
     path('', index, name='index'),
     path('add-harapan', add_harapan, name='add_harapan'),
+    path('json-tembok-harapan', json_tembok_harapan, name='json-tembok-harapan'),
+    path('add-harapan-flutter',add_harapan_flutter, name='add-harapan-flutter'),
 ]
